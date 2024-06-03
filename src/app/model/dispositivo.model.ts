1bb38355b3e8a52a088bb810f1fd967e3ba75d0e
arielg <arielguaman06@gmail.com>
2024-06-03 11:25:28 -0500

    commit a media
    eso mismo

---
+++ src/app/model/dispositivo.model.ts
@@ -0,0 +1,17 @@
+import { Categoria } from "./categoria.model";
+import { Modelo } from "./modelo.model";
+
+export class Dipositivo {
+    id_dispositivo?: number;
+    nombre?: String;
+    numero_serie?: String;
+    disponible?:boolean;
+    categoria?:Categoria;
+    modelo?:Modelo;
+    
+
+
+
+
+    
+}   
---
+++ src/app/model/modelo.model.ts
@@ -0,0 +1,12 @@
+import { Dipositivo } from "./dispositivo.model";
+import { Marca } from "./marca.model";
+
+export class Modelo{
+    id_modelo?:number;
+    nombre?:String;
+    marca?:Marca;
+    dispositivos?:Dipositivo[];
+    
+
+
+}
