02f69d1f1ca0dedb342882a273ea87674ad8f6e5
David Guambaña <david.guambana.est@tecazuay.edu.ec>
2024-06-03 00:09:32 -0500

    integracion mapa

--- src/app/views/mapa/mapa.component.ts
+++ src/app/views/mapa/mapa.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 
+import {GoogleMap, MapHeatmapLayer} from '@angular/google-maps';
+
 @Component({
   selector: 'app-mapa',
   standalone: true,
-  imports: [],
   templateUrl: './mapa.component.html',
-  styleUrl: './mapa.component.scss'
+  styleUrl: './mapa.component.scss',
+  imports: [
+    GoogleMap, MapHeatmapLayer
+  ],
 })
 export class MapaComponent {
-
+  center = {lat: 37.774546, lng: -122.433523};
+  zoom = 12;
+  heatmapOptions = {radius: 5};
+  heatmapData = [
+    {lat: 37.782, lng: -122.447},
+    {lat: 37.782, lng: -122.445},
+    {lat: 37.782, lng: -122.443},
+    {lat: 37.782, lng: -122.441},
+    {lat: 37.782, lng: -122.439},
+    {lat: 37.782, lng: -122.437},
+    {lat: 37.782, lng: -122.435},
+    {lat: 37.785, lng: -122.447},
+    {lat: 37.785, lng: -122.445},
+    {lat: 37.785, lng: -122.443},
+    {lat: 37.785, lng: -122.441},
+    {lat: 37.785, lng: -122.439},
+    {lat: 37.785, lng: -122.437},
+    {lat: 37.785, lng: -122.435}
+  ];
 }
