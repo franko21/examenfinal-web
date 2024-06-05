dfabf532364e2f1a8a5e7bb7b98f2a45c84ce6e8
JosiasAldaz <josiasjoestar@hotmail.com>
2024-06-04 21:31:21 -0500

    marcador en el mapa

--- src/app/views/mapa/mapa.component.ts
+++ src/app/views/mapa/mapa.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component} from '@angular/core';
-
+import { Component, ViewChild} from '@angular/core';
 import {GoogleMap, MapHeatmapLayer, MapMarker} from '@angular/google-maps';
-
 @Component({
   selector: 'app-mapa',
   standalone: true,
@@ -13,17 +11,38 @@ import {GoogleMap, MapHeatmapLayer, MapMarker} from '@angular/google-maps';
   ],
 })
 export class MapaComponent {
-  center: google.maps.LatLngLiteral = { lat: 24, lng: 12 };
-  zoom = 4;
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  center: google.maps.LatLngLiteral = { lat: -2.879767894744873, lng: -78.97490692138672};
+  zoom = 12;
   markerOptions: google.maps.MarkerOptions = { draggable: false };
   markerPositions: google.maps.LatLngLiteral[] = [];
   lastClickedPosition: google.maps.LatLngLiteral | null = null;
+  marker: google.maps.Marker | null = null;
 
   addMarker(event: google.maps.MapMouseEvent) {
     if (event.latLng) {
       const position = event.latLng.toJSON();
-      this.markerPositions.push(position);
-      this.lastClickedPosition = position;
+      this.marker = new google.maps.Marker({
+        position: position,
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 10, 
+          strokeColor: '#f00', 
+          strokeWeight: 5, 
+          fillColor: '#000A02', 
+          fillOpacity: 1
+        },
+        map: null
+      });
+      this.handleMarker(this.marker);
+    }
+  }
+
+  handleMarker(marker: google.maps.Marker) {
+    if (this.map && this.map.googleMap) {
+      marker.setMap(this.map.googleMap);
+    } else {
+      console.error('Mapa no inicializado.');
     }
   }
 
