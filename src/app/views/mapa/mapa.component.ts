ea50a94e065f1b985f145d3b69268f8f991ea00e
JosiasAldaz <josiasjoestar@hotmail.com>
2024-06-29 17:37:26 -0500

    modificaciones al mapa de zonas

--- src/app/views/mapa/mapa.component.ts
+++ src/app/views/mapa/mapa.component.ts
@@ -29,15 +29,19 @@ export class MapaComponent implements OnInit, OnDestroy,AfterViewInit  {
   center: google.maps.LatLngLiteral = { lat: -2.879767894744873, lng: -78.97490692138672 };
   zoom = 12;
   //ELEMENTO PARA BUSCAR DIRECCIONES
-  @ViewChild('autocomplete', { static: false }) input: ElementRef | undefined;
+  @ViewChild('autocomplete', { static: false }) input: ElementRef ;
   autocomplete: google.maps.places.Autocomplete | undefined;
+  @ViewChild('map-container', { static: true }) mapElementRef!: ElementRef;
   //ELEMENTO PARA BUSCAR DIRECCIONES
+  //************ */
+  //VARIABLES PARA LOS MARCADORES DE LOS PUNTOS
   markerOptions: google.maps.MarkerOptions = { draggable: false };
   markerPositions: google.maps.LatLngLiteral[] = [];
   lastClickedPosition: google.maps.LatLngLiteral | null = null;
   marker: google.maps.Marker | null = null;
   listadoPuntos: any[] = [];
   verticesPoligono = [];
+  //VARIBLES PARA LOS MARCADORES DE LOS PUNTOS
   posiciones: Posicion[] = [];
   private posicionSubscription: Subscription;
   mostrarZonas: boolean = true;
@@ -55,7 +59,7 @@ export class MapaComponent implements OnInit, OnDestroy,AfterViewInit  {
   ngAfterViewInit(): void {
     this.zone.runOutsideAngular(() => {
       console.log('vista cargado correctamente con sus child');
-      this.initAutocomplete();
+      this.InitCompletado();
     });
   }
 
@@ -69,7 +73,7 @@ export class MapaComponent implements OnInit, OnDestroy,AfterViewInit  {
     }
   }
 
-  private initAutocomplete(): void {
+  private InitCompletado(): void {
     if (this.input) {
       console.log('CARGA DE DIRECCIONES INICIADA');
       this.autocomplete = new google.maps.places.Autocomplete(this.input.nativeElement, {
@@ -79,21 +83,31 @@ export class MapaComponent implements OnInit, OnDestroy,AfterViewInit  {
       });
 
       this.autocomplete.addListener('place_changed', () => {
-        this.onPlaceChanged();
+        this.CambiarLugar();
       });
     }
   }
 
-  onPlaceChanged(): void {
+  CambiarLugar(): void {
     if (this.autocomplete) {
       const place = this.autocomplete.getPlace();
       if (!place.geometry) {
         // Handle invalid place
+        this.input.nativeElement.placeholder = 'Ingrese una dirección válida';
       } else {
-        console.log(place.name);
+        const detailsElement = document.getElementById('details');
+      if (detailsElement) {
+        detailsElement.innerHTML = place.name || 'Dirección sin nombre';
+        if(place.geometry.location){
+          this.map.googleMap!.setCenter(place.geometry.location);
+          this.map.googleMap!.setZoom(17);
+          console.log(place.name);
+        }
+        
       }
     }
   }
+}
   
   listarposiciones() {
     this.posicionService.listar().subscribe(
@@ -122,6 +136,7 @@ export class MapaComponent implements OnInit, OnDestroy,AfterViewInit  {
   }
 
   ingresarZona() {
+    // Validación para que una zona tenga mínimo 3 puntos
     if (this.listadoPuntos.length < 3) {
       Swal.fire({
         icon: 'error',
@@ -131,61 +146,98 @@ export class MapaComponent implements OnInit, OnDestroy,AfterViewInit  {
         confirmButtonText: 'OK'
       });
     } else {
-      const zona = new Zona_segura();
-      zona.descripcion = "Zona del ISTA";
-      this.zonaService.crear(zona).subscribe({
-        next: (data) => {
-          console.log(data.id_zona_segura);
-          this.listadoPuntos.forEach(punto => {
-            punto.zona_segura = data;
-          });
-          this.IngresarPuntos();
-          Swal.fire({
-            icon: 'success',
-            title: 'Zona segura ingresada correctamente',
-            text: 'Realizado.',
-            confirmButtonColor: '#3085d6',
-            confirmButtonText: 'OK'
-          });
+      // Pedir el nombre de la zona segura
+      Swal.fire({
+        title: 'Ingrese el nombre de la zona segura',
+        input: 'text',
+        inputPlaceholder: 'Nombre de la zona segura',
+        showCancelButton: true,
+        confirmButtonText: 'Guardar',
+        showLoaderOnConfirm: true,
+        preConfirm: (zoneName) => {
+          if (!zoneName) {
+            Swal.showValidationMessage('Debe ingresar un nombre para la zona segura');
+          }
+          return zoneName;
         },
-        error: (err) => {
-          Swal.fire({
-            icon: 'error',
-            title: 'Error al ingresar la zona segura',
-            text: err.message || 'Ocurrió un error desconocido.',
-            confirmButtonColor: '#d33',
-            confirmButtonText: 'OK'
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed) {
+          const zoneName = result.value;
+          const zona = new Zona_segura();
+          zona.descripcion = zoneName;
+          this.zonaService.crear(zona).subscribe({
+            next: (data) => {
+              console.log(data.id_zona_segura);
+              this.listadoPuntos.forEach(punto => {
+                punto.zona_segura = data;
+              });
+              this.IngresarPuntos();
+              Swal.fire({
+                icon: 'success',
+                title: 'Zona segura ingresada correctamente',
+                text: `Nombre de la zona segura: ${zoneName}`,
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'OK'
+              });
+            },
+            error: (err) => {
+              Swal.fire({
+                icon: 'error',
+                title: 'Error al ingresar la zona segura',
+                text: err.message || 'Ocurrió un error desconocido.',
+                confirmButtonColor: '#d33',
+                confirmButtonText: 'OK'
+              });
+            }
           });
         }
       });
     }
   }
 
+  //MÉTODO PARA INGRESAR LOS PUNTOS DE LA ZONA SEGURA
   IngresarPuntos() {
+  let puntosIngresadosCorrectamente = true;
+  let contadorPuntosIngresados = 0;
     this.listadoPuntos.forEach(punto => {
       this.puntoService.crear(punto).subscribe({
         next: (data) => {
-          data.id_punto;
-          const mapContainer = this.map.googleMap;
-          if (mapContainer) {
-            const vertices = this.listadoPuntos.map(punto => ({
-              lat: punto.latitud,
-              lng: punto.longitud
-            }));
-            const poligono = new google.maps.Polygon({
-              paths: vertices,
-              map: mapContainer,
-              strokeColor: '#FF0000',
-              fillColor: '#B9D7FF',
-              strokeWeight: 4,
-            });
+          contadorPuntosIngresados++;
+          if (contadorPuntosIngresados === this.listadoPuntos.length && puntosIngresadosCorrectamente) {
+            console.log('Todos los puntos fueron ingresados correctamente en la base de datos');
+            this.crearPoligono();
           }
         },
         error: (err) => {
           console.error(err);
+          puntosIngresadosCorrectamente = false;
         }
       });
     });
+    //VALIDAR SI TODOS LOS PUNTOS FUERON INGRESADOS CORRECTAMENTE
+    
+  }
+
+  //MÉTODO PARA CREAR EL POLÍGONO DE LA ZONA SEGURA
+  crearPoligono() {
+    const mapContainer = this.map.googleMap;
+    if (mapContainer) {
+      console.log("SE INICIO LA CREACIÓN DE");
+      const vertices = this.listadoPuntos.map(punto => ({
+        lat: punto.latitud,
+        lng: punto.longitud
+      }));
+      const poligono = new google.maps.Polygon({
+        paths: vertices,
+        map: mapContainer,
+        strokeColor: '#759192',
+        fillColor: '#92afb0',
+        strokeWeight: 4,
+      });
+    }else{
+      console.log("NO SE PUDO INICIAR LA CREACIÓN DE");
+    }
   }
 
   actualizarMarcadorEnMapa(position: google.maps.LatLngLiteral) {
